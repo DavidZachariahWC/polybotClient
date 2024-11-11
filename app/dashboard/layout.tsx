f74f299bba6e93cf2700f25df9959012e5a79acb
David <dzach726@gmail.com>
2024-11-10 23:08:19 -0600

    remove dashboard side bar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react"
-import DashboardSideBar from "./_components/dashboard-side-bar"
 import DashboardTopNav from "./_components/dashbord-top-nav"
 import { isAuthorized } from "@/utils/data/user/isAuthorized"
 import { redirect } from "next/dist/server/api-utils"
@@ -16,7 +15,6 @@ export default async function DashboardLayout({ children }: { children: ReactNod
   return (
     <ConversationProvider>
       <div className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
-        <DashboardSideBar />
         <DashboardTopNav >
           <main className="flex flex-col gap-4 p-4 lg:gap-6">
             {children}
